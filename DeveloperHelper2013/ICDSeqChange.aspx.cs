66efcfa18fd967d1a83a27b51d9aafc6ef2903f5
Jayraj17 <goswamijayraj@gmail.com>
2016-07-07 16:34:48 +0530

    test

--- DeveloperHelper2013/ICDSeqChange.aspx.cs
+++ DeveloperHelper2013/ICDSeqChange.aspx.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Web;
+using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -14,7 +16,30 @@ namespace DeveloperHelper2013
         protected void Page_Load(object sender, EventArgs e)
         {
             DBCommunication obj = new DBCommunication();
-            DataTable dt = obj.GetICD();
+           bool test =obj.ChangeSeq("007",8);
+
         }
+
+
+        [WebMethod]
+        public static string GetICDa()
+        {           
+            DBCommunication obj = new DBCommunication();
+            DataTable dt = obj.GetICD();          
+            string json = JsonConvert.SerializeObject(dt, Formatting.Indented);
+            return json;
+        }
+         [WebMethod]
+        public static string ChangeSeq(Dictionary<string, string> SendObj)
+        {
+            string Res="";
+           DBCommunication obj = new DBCommunication();
+           bool Result = Convert.ToBoolean(obj.ChangeSeq(SendObj["ICD"], Convert.ToInt32(SendObj["Seq"])));
+           return Res;
+        }
+
+
+        
+
     }
 }
