95faed1d193e6ce39e531720a803310f4aad87cc
Jayraj17 <goswamijayraj@gmail.com>
2016-07-07 12:00:09 +0530

    test66

--- DeveloperHelper2013/ICDSeqChange.aspx.cs
+++ DeveloperHelper2013/ICDSeqChange.aspx.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
 namespace DeveloperHelper2013
 {
-    public partial class _Default : Page
+    public partial class ICDSeqChange : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            DBCommunication obj = new DBCommunication();
+            DataTable dt = obj.GetICD();
         }
     }
 }
