7d47490797ca507b2a5c58f2c5d3c34f3360d360
Jayraj17 <goswamijayraj@gmail.com>
2016-07-26 17:04:22 +0530

    test

--- DeveloperHelper2013/CropImgSample.aspx.cs
+++ DeveloperHelper2013/CropImgSample.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace DeveloperHelper2013
 {
-    public partial class About : Page
+    public partial class CropImgSample : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
--- DeveloperHelper2013/RadioButtonSaveList.aspx.cs
+++ DeveloperHelper2013/RadioButtonSaveList.aspx.cs
@@ -12,6 +12,7 @@ namespace DeveloperHelper2013
         protected void Page_Load(object sender, EventArgs e)
         {
 
+
         }
     }
 }
