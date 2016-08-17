925e1048ae8f1288b7b1363febfe00794ddf675b
Jayraj17 <goswamijayraj@gmail.com>
2016-08-17 16:16:34 +0530

    te

--- DeveloperHelper2013/ICDSeqChange001.aspx.cs
+++ DeveloperHelper2013/ICDSeqChange001.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace DeveloperHelper2013
 {
-    public partial class test77 : System.Web.UI.Page
+    public partial class ICDSeqChange001 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
