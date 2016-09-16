dafb0ea21452146f58a221f85c40c7ffafa65209
Jayraj17 <goswami_jayraj@yahoo.co.in>
2016-09-16 12:38:48 +0530

    test

---
+++ SampleWebApi/Models/CityModel/City.cs
@@ -0,0 +1,117 @@
+﻿using SampleMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleWebApi.Models.CityModel
+{
+    public class City : ICity
+    {
+        private CityDTO ObjCityDTO = new CityDTO();
+
+
+
+        public CityDTO SaveCity(CityDTO obj)
+        {
+            using (GeesemedLocalEntities DB = new GeesemedLocalEntities())
+            {
+                using (var dbContextTransaction = DB.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        DB.Configuration.ProxyCreationEnabled = false;
+                        DB.Configuration.LazyLoadingEnabled = false;
+                        if (obj == null)
+                        {
+                            throw new ArgumentNullException("item");
+                        }
+                        DB.InsertCity(0,obj.CountryID,obj.CityName);                       
+                        DB.SaveChanges();
+                        dbContextTransaction.Commit();
+                        return obj;
+                    }
+                    catch (Exception)
+                    {
+                        dbContextTransaction.Rollback();
+                        throw;
+                    }
+
+                }
+            }
+        }
+        public bool UpdateCity(CityDTO obj)
+        {
+            using (GeesemedLocalEntities DB = new GeesemedLocalEntities())
+            {
+                using (var dbContextTransaction = DB.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        DB.Configuration.ProxyCreationEnabled = false;
+                        DB.Configuration.LazyLoadingEnabled = false;
+                        if (obj == null)
+                        {
+                            throw new ArgumentNullException("item");
+                        }
+                        DB.InsertCity(obj.CID,obj.CountryID,obj.CityName);
+                        DB.SaveChanges();
+                        dbContextTransaction.Commit();
+                        return true;
+                    }
+                    catch (Exception)
+                    {
+                        dbContextTransaction.Rollback();
+                        throw;
+                    }
+
+                }
+            }
+        }
+        public IEnumerable<CityDTO> GetAll()
+        {
+            using (GeesemedLocalEntities DB = new GeesemedLocalEntities())
+            {
+                DB.Configuration.ProxyCreationEnabled = false;
+                DB.Configuration.LazyLoadingEnabled = false;
+                var CityData = ConvertObjectToData(0);
+                if (CityData != null)
+                {
+                    ObjCityDTO.CityGrid = CityData;
+                    return CityData;
+                }
+                return null;
+            }
+        }
+        public CityDTO Get(int id)
+        {
+            using (GeesemedLocalEntities DB = new GeesemedLocalEntities())
+            {
+                DB.Configuration.ProxyCreationEnabled = false;
+                DB.Configuration.LazyLoadingEnabled = false;
+                var CityData = ConvertObjectToData(id);
+                if (CityData != null)
+                {
+                    ObjCityDTO.CityEdit = CityData.Single();
+                    return CityData.Single();
+                }
+                return null;
+            }
+        }
+        public IEnumerable<CityDTO> ConvertObjectToData(int id)
+        {
+            using (GeesemedLocalEntities DB = new GeesemedLocalEntities())
+            {
+                var Result = from result in DB.GetCity(id).ToList()
+                             select new CityDTO
+                             {
+                                 CityName=result.CityName,                              
+                                 CID = result.CID, 
+                                 CountryDTO = new CountryDTO { CountryName = result.CountryName}
+                             };
+
+                return Result;
+            }
+        }
+    }
+}
