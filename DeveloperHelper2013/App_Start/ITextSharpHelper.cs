3b4358ec414e3492eee7f388ca7216a224fa703f
Jayraj17 <goswami_jayraj@yahoo.co.in>
2016-09-16 13:08:03 +0530

    te

--- DeveloperHelper2013/App_Start/ITextSharpHelper.cs
+++ DeveloperHelper2013/App_Start/ITextSharpHelper.cs
@@ -7,5 +7,357 @@ namespace DeveloperHelper2013.App_Start
 {
     public class ITextSharpHelper
     {
+        public void GetPDFCCMPatMonthlyRpt(DataSet Ds, string PdfTitle)
+        {
+
+
+            MemoryStream stream = new MemoryStream();
+            stream = CCMPDFDATA(Ds, PdfTitle);
+            // Download PDF
+            System.Web.HttpContext.Current.Response.ContentType = "application/pdf";
+            System.Web.HttpContext.Current.Response.AppendHeader("content-disposition", "attachment;filename=" + PdfTitle + ".PDF");
+            System.Web.HttpContext.Current.Response.BinaryWrite(stream.ToArray());
+            System.Web.HttpContext.Current.Response.End();
+
+        }
+
+        private MemoryStream CCMPDFDATA(DataSet Ds, string PdfTitle)
+        {
+            PdfDiv DvHeaderMain = new PdfDiv();
+
+            PdfDiv DvHeaderHead = new PdfDiv();
+            PdfDiv DvHeaderHeadSub = new PdfDiv();
+            PdfDiv DvHeaderLeft = new PdfDiv();
+            PdfDiv DvSubHeaderLeft = new PdfDiv();
+            PdfDiv DvHeaderRight = new PdfDiv();
+            PdfDiv DvHeaderLogo = new PdfDiv();
+            PdfDiv DvData = new PdfDiv();
+
+
+            ///LOGO
+            ///
+            iTextSharp.text.Image logoimg = null;
+            logoimg = iTextSharp.text.Image.GetInstance(System.Web.HttpContext.Current.Server.MapPath("~/App_Themes/NewTheme/Images/Geesemed_logo.jpg"));
+            logoimg.ScaleToFit(80f, 80f);
+            logoimg.Alignment = iTextSharp.text.Image.ALIGN_RIGHT;
+
+            DvHeaderLogo.AddElement(logoimg);
+            DvHeaderLogo.TextAlignment = Element.ALIGN_RIGHT;
+
+
+            MemoryStream ms = new MemoryStream();
+            iTextSharp.text.Rectangle rect = PageSize.A4;
+            Document CCMDoc = new Document(rect, 25f, 25f, 25f, 40f);
+            PdfWriter writer_Sub = PdfWriter.GetInstance(CCMDoc, ms);
+
+            writer_Sub.PageEvent = new PDFDashBoardFooter();
+
+            CCMDoc.Open();
+
+            DvHeaderMain.AddElement(logoimg);
+
+
+            ClientMasterDAL objClient = new ClientMasterDAL(SessionManager.CONNECTIONSTRING);
+            DataTable dtHead = new DataTable();
+            dtHead = objClient.GetClient_Master(SessionManager.CLIENT_ID);
+            string CliName = "";
+            string CliAdd = "";
+            string CityState = "";
+            string PhoneFax = "";
+            string City = "";
+            string State = "";
+            string Zip = "";
+            string Phone = "";
+            string FAX = "";
+
+            if (dtHead.Rows.Count != 0)
+            {
+                CliName = Convert.ToString(dtHead.Rows[0]["mclnt_name"]);
+                CliAdd = Convert.ToString(dtHead.Rows[0]["mdl_add1"]) + " " + Convert.ToString(dtHead.Rows[0]["mdl_add2"]);
+                City = Convert.ToString(dtHead.Rows[0]["city_name"]);
+                State = Convert.ToString(dtHead.Rows[0]["state_code"]);
+                Zip = Convert.ToString(dtHead.Rows[0]["zip_code"]);
+                Phone = Convert.ToString(dtHead.Rows[0]["mclnt_cont_phone"]);
+                FAX = Convert.ToString(dtHead.Rows[0]["mclnt_fax"]);
+            }
+
+
+
+
+            if (City != "")
+            {
+                CityState += City;
+            }
+            if (State != "")
+            {
+                CityState += (City != "" ? ", " : "") + State;
+            }
+            if (Zip != "")
+            {
+                CityState += " " + Zip;
+            }
+            if (Phone != "")
+            {
+                PhoneFax += "(T): " + Phone;
+            }
+            if (FAX != "")
+            {
+                PhoneFax += " (F): " + FAX;
+            }
+
+            //LEFT
+            string PatName = string.Empty;
+            string BDate = string.Empty;
+            string Gender = string.Empty;
+            if (Ds.Tables[0].Rows.Count != 0)
+            {
+                PatName = Ds.Tables[0].Rows[0]["mpt_pat_name"].ToString();
+                BDate = Ds.Tables[0].Rows[0]["BDate"].ToString();
+                Gender = Ds.Tables[0].Rows[0]["mpt_pat_sex"].ToString();
+            }
+            clsHospital objHosp = new clsHospital(SessionManager.CONNECTIONSTRING, SessionManager.HOSPITAL_ID);
+            string hospName = objHosp.getHospitalName();
+
+
+            DvHeaderLeft.Float = PdfDiv.FloatType.LEFT;
+
+
+            DvHeaderLeft.AddElement(new Chunk(hospName + "\n", xPDF.GetCommonSubTitle_Chart()));
+            DvHeaderLeft.AddElement(new Chunk(CliName + "\n", xPDF.GetCommonSubTitle_Chart()));
+            DvHeaderLeft.AddElement(new Chunk(CliAdd + "\n", xPDF.GetCommonSubTitle_Chart()));
+            DvHeaderLeft.AddElement(new Chunk(CityState + "\n", xPDF.GetCommonSubTitle_Chart()));
+            DvHeaderLeft.AddElement(new Chunk(PhoneFax + "\n", xPDF.GetCommonSubTitle_Chart()));
+
+
+            DvHeaderLeft.Width = 180f;
+            DvHeaderLeft.PaddingTop = 10f;
+            DvHeaderMain.AddElement(DvHeaderLeft);
+
+
+            //RIGHT
+            PdfPTable PtableRight = new PdfPTable(1);
+            DvHeaderRight.Float = PdfDiv.FloatType.RIGHT;
+
+
+            PdfPCell cell = new PdfPCell(new Phrase(new Chunk(PdfTitle, xPDF.GetCommonSubTitle_Chart())));
+            cell.Border = 0;
+            cell.HorizontalAlignment = PdfPCell.ALIGN_RIGHT;
+            PtableRight.AddCell(cell);
+
+
+
+
+
+            DvHeaderRight.PaddingTop = 10f;
+            DvHeaderRight.Width = 290f;
+            DvHeaderRight.AddElement(PtableRight);
+
+            DvHeaderMain.AddElement(DvHeaderRight);
+
+            ///SUB Head
+
+
+
+            DvSubHeaderLeft.AddElement(new Chunk(new LineSeparator(0.8f, 100f, BaseColor.GRAY, Element.ALIGN_CENTER, 1)));
+
+
+
+            DvSubHeaderLeft.AddElement(new Chunk("Patient :- " + PatName + "\n", CCMReportCls.GetCommonSubTitle_Chart()));
+            DvSubHeaderLeft.AddElement(new Chunk("DOB :- " + BDate + "\n", CCMReportCls.GetCommonSubTitle_Chart()));
+            DvSubHeaderLeft.AddElement(new Chunk("Gender :- " + Gender + "\n", CCMReportCls.GetCommonSubTitle_Chart()));
+            DvSubHeaderLeft.AddElement(new Chunk("Physician :- " + Ds.Tables[1].Rows[0]["ProName"].ToString() + "\n", CCMReportCls.GetCommonSubTitle_Chart()));
+
+
+
+
+            DvSubHeaderLeft.Width = 540f;
+            //   DvSubHeaderLeft.PercentageWidth = 80f;
+            DvHeaderMain.AddElement(DvSubHeaderLeft);
+
+
+            CCMDoc.Add(DvHeaderMain);
+            //DvData.AddElement(ExportToPdf(Ds));
+            ExportToPdf(Ds, ref CCMDoc);
+            //DvHeaderMain.AddElement(DvData);
+
+
+
+            CCMDoc.Close();
+
+            return ms;
+        }
+        public void ExportToPdf(DataSet ds, ref Document CCMDoc)
+        {
+            PdfPTable PdfTable = new PdfPTable(1);
+            PdfTable.WidthPercentage = 98f;
+            PdfTable.DefaultCell.Border = 1;
+            PdfPCell PdfPCell = null;
+            PdfPCell PdfPCellMain = null;
+            PdfPCell PdfPCellSub = null;
+            int CarGrdID = 0;
+
+
+
+            PdfTable.SplitLate = false;
+            PdfTable.SplitRows = true;
+
+
+            PdfTable.DefaultCell.Border = 0;
+            PdfTable.AddCell(new Phrase(new Chunk(new LineSeparator(0.8f, 100f, BaseColor.GRAY, Element.ALIGN_CENTER, 1))));
+
+            for (int i = 0; i < ds.Tables[1].Rows.Count; i++)
+            {
+
+                if (CarGrdID != Convert.ToInt32(ds.Tables[1].Rows[i]["TitleGroupID"]))
+                {
+
+                    PdfPCellMain = new PdfPCell(new Phrase(new Chunk(ds.Tables[1].Rows[i]["TitleGroupName"].ToString() + "\n\n", CCMReportCls.GetHead())));
+                    PdfPCellMain.Border = 0;
+                    PdfTable.AddCell(PdfPCellMain);
+
+
+
+                    PdfPCellSub = new PdfPCell(new Phrase(new Chunk(ds.Tables[1].Rows[i]["TitleDisplayName"].ToString(), CCMReportCls.GetSubHead())));
+                    PdfPCellSub.Border = 0;
+                    PdfTable.AddCell(PdfPCellSub);
+                    PdfPCell = new PdfPCell(new Phrase(new Chunk(ds.Tables[1].Rows[i]["TitleValue"].ToString() + "\n\n", CCMReportCls.GetDetials())));
+                    PdfPCell.Border = 0;
+                    PdfTable.AddCell(PdfPCell);
+
+
+
+                }
+                else
+                {
+                    PdfPCellSub = new PdfPCell(new Phrase(new Chunk(ds.Tables[1].Rows[i]["TitleDisplayName"].ToString(), CCMReportCls.GetSubHead())));
+                    PdfPCellSub.Border = 0;
+                    PdfTable.AddCell(PdfPCellSub);
+                    PdfPCell = new PdfPCell(new Phrase(new Chunk(ds.Tables[1].Rows[i]["TitleValue"].ToString() + "\n\n", CCMReportCls.GetDetials())));
+                    PdfPCell.Border = 0;
+                    PdfTable.AddCell(PdfPCell);
+                }
+
+                CarGrdID = Convert.ToInt32(ds.Tables[1].Rows[i]["TitleGroupID"]);
+            }
+
+
+            ///////////////////////////////////////////////////////////////CCM DATA
+            int CCMGrdID = 0;
+            for (int i = 0; i < ds.Tables[2].Rows.Count; i++)
+            {
+
+                if (CCMGrdID != Convert.ToInt32(ds.Tables[2].Rows[i]["TitleGroupID"]))
+                {
+                    PdfPCellMain = new PdfPCell(new Phrase(new Chunk(ds.Tables[2].Rows[i]["TitleGroupName"].ToString() + "\n\n", CCMReportCls.GetHead())));
+                    PdfPCellMain.Border = 0;
+                    PdfTable.AddCell(PdfPCellMain);
+
+
+
+                    PdfPCellSub = new PdfPCell(new Phrase(new Chunk(ds.Tables[2].Rows[i]["TitleDisplayName"].ToString(), CCMReportCls.GetSubHead())));
+                    PdfPCellSub.Border = 0;
+                    PdfTable.AddCell(PdfPCellSub);
+                    PdfPCell = new PdfPCell(new Phrase(new Chunk(ds.Tables[2].Rows[i]["TitleValue"].ToString() + "\n\n", CCMReportCls.GetDetials())));
+                    PdfPCell.Border = 0;
+                    PdfTable.AddCell(PdfPCell);
+
+
+
+                }
+                else
+                {
+                    PdfPCellSub = new PdfPCell(new Phrase(new Chunk(ds.Tables[2].Rows[i]["TitleDisplayName"].ToString(), CCMReportCls.GetSubHead())));
+                    PdfPCellSub.Border = 0;
+                    PdfTable.AddCell(PdfPCellSub);
+                    PdfPCell = new PdfPCell(new Phrase(new Chunk(ds.Tables[2].Rows[i]["TitleValue"].ToString() + "\n\n", CCMReportCls.GetDetials())));
+                    PdfPCell.Border = 0;
+                    PdfTable.AddCell(PdfPCell);
+                }
+
+                CCMGrdID = Convert.ToInt32(ds.Tables[2].Rows[i]["TitleGroupID"]);
+            }
+
+            CCMDoc.Add(PdfTable);
+        }
+
+
+        #region PDF - FONT
+
+
+        public static Font GetHead()
+        {
+            Font CommonFont = new Font(Font.GetFamilyIndex("Arial"), 16f, Font.BOLD, new iTextSharp.text.BaseColor(87, 85, 85));
+            return CommonFont;
+        }
+        public static Font GetSubHead()
+        {
+            Font CommonFont = new Font(Font.GetFamilyIndex("Arial"), 12f, Font.BOLD, new iTextSharp.text.BaseColor(87, 85, 85));
+            return CommonFont;
+        }
+        public static Font GetDetials()
+        {
+            Font CommonFont = new Font(Font.GetFamilyIndex("Arial"), 10f, Font.NORMAL, new iTextSharp.text.BaseColor(87, 85, 85));
+            return CommonFont;
+        }
+        public static Font GetBoldFont(float size)
+        {
+            size = 10f; // changed on 07/28/2015 - Changed from Dynamic
+            Font retObj = new Font();
+            retObj.SetStyle("Italic");
+            retObj.Size = size;
+            return retObj;
+        }
+        public static Font GetCommonFont()
+        {
+            Font CommonFont = new Font(Font.GetFamilyIndex("Arial"), 10f, Font.NORMAL, new iTextSharp.text.BaseColor(87, 85, 85));
+            return CommonFont;
+        }
+        public static Font GetCommonFontHead()
+        {
+            Font CommonFont = new Font(Font.GetFamilyIndex("Arial"), 10f, Font.BOLD, new iTextSharp.text.BaseColor(87, 85, 85));
+            return CommonFont;
+        }
+        public static Font GetCommonFont(float size)
+        {
+            size = 10f; // changed on 07/28/2015 - Changed from Dynamic
+            Font CommonFont = new Font(Font.GetFamilyIndex("Arial"), size, Font.NORMAL, new iTextSharp.text.BaseColor(87, 85, 85));
+            return CommonFont;
+        }
+
+        //CHART 
+        public static Font GetCommonFont_Chart()
+        {
+            iTextSharp.text.Font docf = FontFactory.GetFont(FontFactory.HELVETICA, 10, new iTextSharp.text.BaseColor(87, 85, 85));
+            return docf;
+        }
+        public static Font GetCommonFontBold_Chart()
+        {
+            iTextSharp.text.Font docf = FontFactory.GetFont(FontFactory.HELVETICA, 10, iTextSharp.text.Font.BOLD, new iTextSharp.text.BaseColor(87, 85, 85));
+            return docf;
+        }
+        public static Font GetCommonTitle_Chart()
+        {
+            iTextSharp.text.Font hf = FontFactory.GetFont(FontFactory.HELVETICA, 14, iTextSharp.text.Font.BOLD, BaseColor.GRAY);
+            return hf;
+        }
+        public static Font GetCommonSubTitle_Chart()
+        {
+            iTextSharp.text.Font hf = FontFactory.GetFont(FontFactory.HELVETICA, 10, BaseColor.GRAY);
+            return hf;
+        }
+        public static Font GetCommonPatDetails_Chart()
+        {
+            iTextSharp.text.Font docf = FontFactory.GetFont(FontFactory.HELVETICA, 10, BaseColor.GRAY);
+            return docf;
+        }
+        public static Font GetCommonFontBold_Chart_Gray()
+        {
+            iTextSharp.text.Font docf = FontFactory.GetFont(FontFactory.HELVETICA, 10, iTextSharp.text.Font.BOLD, BaseColor.GRAY);
+            return docf;
+        }
+
+
+
+        #endregion
     }
 }
---
+++ DeveloperHelper2013/Password.aspx
@@ -0,0 +1,133 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Password.aspx.cs" Inherits="DeveloperHelper2013.Password" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    
+
+           <div class="form-group has-success has-feedback">
+                                <label>Password:</label>
+                                <input type="password" class="form-control" placeholder="Password" id="txtPassword" />
+                                 <span id="spnPassword" class="glyphicon glyphicon-eye-open cursor-pointer form-control-feedback" title="Show Password" style="pointer-events: all;"></span>
+                                <div id="PwdDetails" class="passverify" style="display: none; font-size: 12px;">
+                                    <ul style="list-style-type: none; padding: 0;">
+                                        <li id="length" class="invalid"><i class="glyphicon glyphicon-remove-circle"></i>Have at least <strong>5 characters</strong></li>
+                                        <li id="lengthMax" class="invalid"><i class="glyphicon glyphicon-remove-circle"></i>Have maximum <strong>15 characters</strong></li>
+                                        <li id="number" class="invalid"><i class="glyphicon glyphicon-remove-circle"></i>Have at least <strong>one number</strong></li>
+                                        <li id="Special" class="invalid"><i class="glyphicon glyphicon-remove-circle"></i>Have at least One <strong>Special character </strong></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+
+        <script>
+
+
+            
+       $(document).ready(function () {
+           $('#txtPassword').popover({
+               trigger: 'focus',
+               placement: 'right',
+               html: true,
+               width: 500,
+               container: 'body',
+               content: function () {
+                   var message = $("#PwdDetails").html();
+                   return message;
+               }
+           });
+           BindUserMaster();
+           BindRoleMaster();
+
+       });
+            $('#txtPassword').keyup(function () {
+                try {
+                    CheckPwd($("#txtPassword").val());
+                    var id = $(this).attr("aria-describedby");
+                    $("#" + id).find(".popover-content").html($("#PwdDetails").html());
+                } catch (err) {
+                    alert(err);
+                }
+            });      
+            $('#spnPassword').click(function () {           
+                var $pwd = $("#txtPassword");
+                if ($pwd.attr('type') === 'password') {
+                    $pwd.attr('type', 'text');
+                    $(this).addClass("glyphicon-eye-close");             
+                } else {
+                    $pwd.attr('type', 'password');
+                    $(this).removeClass("glyphicon-eye-close");
+                    $(this).addClass("glyphicon-eye-open");              
+                }
+            });
+
+
+
+
+
+
+
+
+
+            function JS_CheckPwd(pswd) {
+                try {
+
+                    var regExPass = /(?=^.{0,15}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?=.*\W.*)(?!.*\s).*$/g;
+                    if (isNaN(regExPass)) {
+                        //validate Minimum length
+                        if (pswd.length >= 5) {
+                            $('#length').removeClass('invalid').addClass('valid');
+                            $('#length i').removeClass('glyphicon-remove-circle').addClass('glyphicon-ok-circle');
+                        } else {
+                            $('#length').removeClass('valid').addClass('invalid');
+                            $('#length i').removeClass('glyphicon-ok-circle').addClass('glyphicon-remove-circle');
+                        }
+                        //validate Maximum length
+                        if (pswd.length > 0 && pswd.length <= 15) {
+                            $('#lengthMax').removeClass('invalid').addClass('valid');
+                            $('#lengthMax i').removeClass('glyphicon-remove-circle').addClass('glyphicon-ok-circle');
+                        } else {
+                            $('#lengthMax').removeClass('valid').addClass('invalid');
+                            $('#lengthMax i').removeClass('glyphicon-ok-circle').addClass('glyphicon-remove-circle');
+                        }
+                        //validate number
+                        if (pswd.match(/\d+/)) {
+                            $('#number').removeClass('invalid').addClass('valid');
+                            $('#number i').removeClass('glyphicon-remove-circle').addClass('glyphicon-ok-circle');
+                        } else {
+                            $('#number').removeClass('valid').addClass('invalid');
+                            $('#number i').removeClass('glyphicon-ok-circle').addClass('glyphicon-remove-circle');
+                        }
+                        var Special_Char = /[!@#$%^&*()_+}{:;'?/><,]/g;
+                        if (isNaN(pswd)) {
+                            if (pswd.match(Special_Char)) {
+                                $('#Special').removeClass('invalid').addClass('valid');
+                                $('#Special i').removeClass('glyphicon-remove-circle').addClass('glyphicon-ok-circle');
+                            }
+                            else {
+                                $('#Special').removeClass('valid').addClass('invalid');
+                                $('#Special i').removeClass('glyphicon-ok-circle').addClass('glyphicon-remove-circle');
+                            }
+                        }
+                        else {
+                            $('#Special').removeClass('valid').addClass('invalid');
+                            $('#Special i').removeClass('glyphicon-ok-circle').addClass('glyphicon-remove-circle');
+                        }
+                    }
+                } catch (err) {
+                    alert(err);
+                }
+            }
+
+
+        </script>
+
+
+    </form>
+</body>
+</html>
