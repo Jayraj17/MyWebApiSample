5545715e6de9d7f697ff55ebd084e4f2c692ba0b
Jayraj17 <goswami_jayraj@yahoo.co.in>
2016-09-16 12:38:59 +0530

    test

---
+++ SampleWebApi/Models/CityModel/ICity.cs
@@ -0,0 +1,17 @@
+﻿using SampleMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWebApi.Models.CityModel
+{
+    interface ICity
+    {
+        CityDTO SaveCity(CityDTO obj);
+        bool UpdateCity(CityDTO obj);
+        IEnumerable<CityDTO> GetAll();
+        CityDTO Get(int id);
+    }
+}
