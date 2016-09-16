0c2efe0183e53113f0fd19ed4934071bf2eb44b1
Jayraj17 <goswami_jayraj@yahoo.co.in>
2016-09-16 12:19:13 +0530

    test

--- DeveloperHelper2013/App_Start/Startup.Auth.cs
+++ DeveloperHelper2013/App_Start/Startup.Auth.cs
@@ -13,6 +13,7 @@ namespace DeveloperHelper2013
 {
     public partial class Startup {
 
+
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301883
         public void ConfigureAuth(IAppBuilder app)
         {
