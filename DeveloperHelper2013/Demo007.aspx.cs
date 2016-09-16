7ae19afd9d2d016897d2afa0e779f49a56eb20f7
Jayraj17 <goswami_jayraj@yahoo.co.in>
2016-09-16 18:22:22 +0530

    test

--- DeveloperHelper2013/Demo007.aspx.cs
+++ DeveloperHelper2013/Demo007.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace DeveloperHelper2013
 {
-    public partial class Contact : Page
+    public partial class Demo007 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
