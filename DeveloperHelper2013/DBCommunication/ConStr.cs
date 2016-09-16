2b81171e3e0fabbbd0c33a95254f0d6f30d60aa7
Jayraj17 <goswami_jayraj@yahoo.co.in>
2016-09-16 13:06:19 +0530

    test

--- DeveloperHelper2013/DBCommunication/ConStr.cs
+++ DeveloperHelper2013/DBCommunication/ConStr.cs
@@ -14,6 +14,7 @@ namespace DeveloperHelper2013
         SqlDataAdapter da = new SqlDataAdapter();
 
 
+
         public void CreateConnectionObject()
         {
             string constr1 = WebConfigurationManager.ConnectionStrings["DeveloperConnectionString"].ConnectionString;
