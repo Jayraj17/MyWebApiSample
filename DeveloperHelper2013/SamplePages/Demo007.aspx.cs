70af3210b28f1b91adfdcea2c7c3090a15eb7db1
Jayraj17 <goswami_jayraj@yahoo.co.in>
2017-01-10 18:29:03 +0530

    test

--- DeveloperHelper2013/SamplePages/Demo007.aspx.cs
+++ DeveloperHelper2013/SamplePages/Demo007.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml.Linq;
 
 namespace DeveloperHelper2013
 {
@@ -11,7 +12,22 @@ namespace DeveloperHelper2013
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            BindGrid();
         }
+        XDocument xmldoc;
+        public void BindGrid()
+        {
+            xmldoc = XDocument.Load(Server.MapPath("~/XmlDB/Emp.xml"));   //add xml document  
+            var bind = xmldoc.Descendants("Employee").Select(p => new
+            {
+                Id = p.Element("id").Value,
+                Name = p.Element("name").Value,
+                Salary = p.Element("salary").Value,
+                Email = p.Element("email").Value,
+                Address = p.Element("address").Value
+            }).OrderBy(p => p.Id);
+            GridView1.DataSource = bind;
+            GridView1.DataBind();
+        }  
     }
 }
