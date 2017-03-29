94d5cc68a3cbe5df831e8a65a492e0e1f8c15aaf
Jayraj17 <goswami_jayraj@yahoo.co.in>
2017-03-29 16:54:02 +0530

    ccc

---
+++ SampleMapper/Product_DTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleMapper
+{
+    public class Product_DTO
+    {
+        public string ProID { get; set; }
+        public string ProName { get; set; }      
+        public Nullable<System.DateTime> ProExpDate { get; set; }
+        public int UserID { get; set; }
+
+        public IEnumerable<Product_DTO> ProjectGrid { get; set; }
+        public Product_DTO ProjectEdit { get; set; }
+        
+    }
+}
