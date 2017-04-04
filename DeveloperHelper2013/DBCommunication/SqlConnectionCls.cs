b6b3db79f5483a6550c4790af374b954c7bb677c
Jayraj17 <goswami_jayraj@yahoo.co.in>
2017-04-04 16:46:04 +0530

    rrr

---
+++ DeveloperHelper2013/DBCommunication/SqlConnectionCls.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace DeveloperHelper2013.DBCommunication
+{
+    public static class SqlConnectionCls
+    {
+       public  static String ConStr = WebConfigurationManager.ConnectionStrings["DeveloperConnectionString"].ConnectionString;
+    }
+}
