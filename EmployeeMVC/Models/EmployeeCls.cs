0b4426b480c9eb0fd9e0841fc511db244281b5b3
Goswami Jayraj <goswami.jayraj@hotmail.com>
2017-04-10 22:42:58 +0530

    test

--- EmployeeMVC/Models/EmployeeCls.cs
+++ EmployeeMVC/Models/EmployeeCls.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -24,7 +25,62 @@ namespace EmployeeMVC.Models
             Employee obj = db.Employees.Where(a => a.EmpNo == EmpNo).SingleOrDefault();
             return obj;
         }
-    
-    
+
+        public static int AddEmployee(Employee model, int EmpNo = 0)
+        {
+            DeveloperEntities db = new DeveloperEntities();
+            Employee obj = new Employee();
+            bool IsExist = db.Employees.Any(R => R.EmpName == model.EmpName);
+            if (IsExist == true)
+            {
+                return 0;
+            }
+            else
+            {
+                int maxid = db.Employees.Any() ? db.Employees.Max(s => s.EmpNo) : 0;
+                //obj.EmpNo = maxid + 1;
+                obj.EmpName = model.EmpName;
+                obj.Salary = model.Salary;
+                obj.DeptName = model.DeptName;
+                obj.Designation = model.Designation;
+                obj.EmpFile = model.EmpFile;
+                obj.UserName = model.UserName;
+                obj.Password = model.Password;
+                obj.IsActive = 1;
+                db.Employees.Add(obj);
+                return db.SaveChanges();
+            }
+        }
+        public static int UpdateEmployee(Employee model, int EmpNo = 0)
+        {
+
+            DeveloperEntities db = new DeveloperEntities();
+            bool IsExist = db.Employees.Where(P => P.EmpNo != model.EmpNo).Any(R => R.EmpName == model.EmpName);
+            if (IsExist == true)
+            {
+                return 0;
+            }
+            else
+            {               
+               // model.UserId = ESrNo;
+                db.Employees.Attach(model);
+                db.Entry(model).State = EntityState.Modified;                
+                return db.SaveChanges();
+            }
+        }
+        public bool RemoveEmployee(int EmpNo = 0)
+        {
+            DeveloperEntities db = new DeveloperEntities();
+            Employee Obj = db.Employees.Single(t => t.EmpNo == EmpNo);
+            if (Obj != null)
+            {
+                Obj.IsActive=0;
+                db.Entry(Obj).State = EntityState.Modified;  
+                db.SaveChanges();
+            }
+            return true;
+        }
+
+
     }
 }
