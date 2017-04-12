2dbd10363d1bb9dc774f83a0b67a8e62df3a0fb6
Goswami Jayraj <goswami.jayraj@hotmail.com>
2017-04-12 06:37:40 +0530

    test88

--- DeveloperHelper2013/DBCommunication/HelperCls.cs
+++ DeveloperHelper2013/DBCommunication/HelperCls.cs
@@ -7,5 +7,18 @@ namespace DeveloperHelper2013
 {
     public class HelperCls
     {
+//        Respected Dimple Madam,
+ 
+//I am  Jayraj Goswami request you to accept this letter as my formal notice of resignation from Crystal Voxx Ltd..
+ 
+//I made this decision, not because I’m unhappy with the opportunities you’ve presented, but as a strategic career move.
+ 
+//Please consider my last working day will be 11-Jun-2017.
+
+
+//Yours Sincerely
+//JAYRAJ B. GOSWAMI
+
+
     }
 }
