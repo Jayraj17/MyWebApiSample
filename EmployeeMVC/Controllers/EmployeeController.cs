32f6cd3cb98fb1347fe80f97d950fec89e1c3d38
Goswami Jayraj <goswami.jayraj@hotmail.com>
2017-04-19 09:01:18 +0530

    test

---
+++ EmployeeMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,76 @@
+﻿using EmployeeMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ManageEmployee()
+        {
+            return View(EmpCls.GetEmployee());
+        }
+        public ActionResult AddEmployee(int EmpNo = 0)
+        {
+           // ViewBag.PCountryDD = Common.GetCountryMst();
+           // ViewBag.PStateDD = Common.GetStateMst();
+           // ViewBag.PCityDD = Common.GetCityMst();
+            Employee obj = new Employee();
+            if (EmpNo != 0)
+            {
+                obj = EmpCls.GetEmployee(EmpNo);
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        public ActionResult AddEmployee(Employee model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.EmpNo == 0)
+                {      
+                     if (EmpCls.AddEmployee(model, model.EmpNo)==0)
+                    {
+                        ViewBag.Message = "Duplicate Name";
+                        Employee obj = new Employee();
+                        return View(obj);
+                     }
+                   
+                }
+                else
+                {
+                    if (EmpCls.UpdateEmployee(model,model.EmpNo) == 0)
+                    {
+                        ViewBag.Message = "Duplicate Name";
+                        Employee obj = new Employee();
+                        return View(obj);
+                    }
+                }
+            }
+            return RedirectToAction("ManageEmployee");
+        }
+        public ActionResult RemoveEmployee(int EmpNo = 0)
+        {
+            Boolean Del;
+            EmpCls CObj = new EmpCls();
+            Del = CObj.RemoveEmployee(EmpNo);
+            if (Del == true)
+            {
+                return RedirectToAction("ManageEmployee");
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+	}
+}
